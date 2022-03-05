ee624d58168286ab95943fded8045fa0827d59cc
jennyhuoh <huoh.li@gmail.com>
2022-03-05 21:19:24 +0800

    adjust the search bar to Formik tools

--- pages/users/[username]/repos.js
+++ pages/users/[username]/repos.js
@@ -1,7 +1,20 @@
-
-import { Box, Input, Flex, Spacer, Button, Center, Stat, StatLabel, StatNumber, StatHelpText, VStack } from '@chakra-ui/react'
+import { Box,
+         Input, 
+         Flex, 
+         Spacer, 
+         Button, 
+         Center, 
+         Stat, 
+         StatLabel, 
+         StatNumber, 
+         StatHelpText, 
+         VStack,
+         FormControl
+         } from '@chakra-ui/react'
+import { Formik, Field, Form } from 'formik';
 
 export default function Repos() {
+    
   return (
     <>
       <Flex bg='gray.800' h='8vh' color='white' px='9'>
@@ -9,14 +22,44 @@ export default function Repos() {
           Intern Homework
         </Box>
         <Spacer />
-        <Box py='4'>
-          <Input placeholder='Search user' size='sm' />
-        </Box>
-        <Box py='4'>
-          <Button borderRadius='0.1vw' border='0.1vw solid white' colorScheme='gray.800' borderLeft='none' size='sm'>
-            Search
-          </Button>
-        </Box>
+        <Formik
+            initialValues={{ name: '' }}
+            onSubmit={(values, {setSubmitting}) => {
+                setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                setSubmitting(false)
+                }, 1000)
+            }}
+        >
+        {(props) => (
+            <Form style={{display:'flex'}}>
+                <Field name='name'>
+                {({ field }) => (
+                    <FormControl>
+                        <Box py='3'>
+                        <Input
+                        {...field} 
+                        id='name'
+                        autoComplete='off' />
+                        </Box>
+                    </FormControl>
+                )}
+                </Field>
+                <Box py='3' pl='1'>
+                    <Button
+                        borderRadius='0.4vw' 
+                        border='1px solid white'
+                        colorScheme='gray.800'
+                        h='4.85vh'
+                        isLoading={props.isSubmitting}
+                        type='submit'
+                    >
+                        Submit
+                    </Button>
+                </Box>
+            </Form>
+        )}
+        </Formik>
       </Flex>
       <Center bg='gray.50' h='10vh' borderBottom='0.1vw solid lightGray' fontWeight='bold' letterSpacing='0.05vw' fontSize='1.4vw'>
         jennyhuoh's Repositories
